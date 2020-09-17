8b3b72625374df8b4cde16e884484b36dcfbca58
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-09-17 15:32:56 -0500

    Added logic to calculate_price function. All tests are passing now.

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -9,7 +9,18 @@ order price.
 
 
 def calculate_price(price, cash_coupon, percent_coupon):
-    pass
+    with_cashcoupon = price - cash_coupon
+    with_percentcoupon = with_cashcoupon - (with_cashcoupon * percent_coupon/100)
+    subtotal_tax = with_percentcoupon * 1.06
+    if with_percentcoupon < 10:
+        shipping = 5.95
+    elif with_percentcoupon < 30:
+        shipping = 7.95
+    elif with_percentcoupon < 50:
+        shipping = 11.95
+    else:
+        shipping = 0
+    return subtotal_tax + shipping
 
 
 if __name__ == '__main__':
