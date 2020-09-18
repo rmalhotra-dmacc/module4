41d10c573c9ba272d530fb0aac306c214581580a
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-09-18 11:37:51 -0500

    Added code to raise ValueError in validation_with_try.py

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -10,6 +10,8 @@ order price.
 
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
+    if score1 < 0 or score2 < 0 or score3 < 0:
+        raise ValueError
     return float((score1 + score2 + score3)/NUMBER_TESTS)
 
 
