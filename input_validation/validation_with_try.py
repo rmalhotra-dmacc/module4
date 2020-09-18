29f5910d5c2a22ba2fbd6c30b9178a73ec310e9e
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-09-18 11:33:24 -0500

    Added validation_with_try.py & test_validation_with_try.py

---
+++ input_validation/validation_with_try.py
@@ -0,0 +1,18 @@
+"""""
+Program: validation_with_try.py
+Author: Rajiv Malhotra
+Last Modified Date: 09/187/2020
+ 
+The program will accept the amount of purchase, the cash coupon, the percentage coupon and it will calculate the total
+order price. 
+"""""
+
+
+def average(score1, score2, score3):
+    NUMBER_TESTS = 3
+    return float((score1 + score2 + score3)/NUMBER_TESTS)
+
+
+if __name__ == '__main__':
+    average_score = average(1, 1, 1)
+    print("Average score is {}".format(average_score))
